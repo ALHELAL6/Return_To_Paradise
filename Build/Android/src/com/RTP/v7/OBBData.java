a1491ad1722ba760d5aa10e8519a5fb1cc15efc1
mag1182 <mag1182@rocketmail.com>
2022-03-16 11:57:49 -0500

    Coins and Border work

--- Build/Android/src/com/RTP/v7/OBBData.java
+++ Build/Android/src/com/RTP/v7/OBBData.java
@@ -18,8 +18,8 @@ mFileSize = fileSize;
 public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
-"1", // the version of the APK that the file was uploaded against
-1687108509L // the length of the file in bytes
+"3", // the version of the APK that the file was uploaded against
+1686848726L // the length of the file in bytes
 )
 };
 };
