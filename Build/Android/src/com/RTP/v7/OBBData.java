106dc4610d9fc299d34789851fb2efbf60d7d6c6
mag1182 <mag1182@rocketmail.com>
2022-04-14 05:48:13 -0500

    Added test map and basic mobile character

--- Build/Android/src/com/RTP/v7/OBBData.java
+++ Build/Android/src/com/RTP/v7/OBBData.java
@@ -19,7 +19,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "3", // the version of the APK that the file was uploaded against
-1686848726L // the length of the file in bytes
+540357380L // the length of the file in bytes
 )
 };
 };
