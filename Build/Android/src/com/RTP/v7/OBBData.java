c16a041c1d9d783446f187397e8ed745299b1c92
mag1182 <mag1182@rocketmail.com>
2022-05-04 23:14:58 -0500

    Patched Null Pointer Bug
    The grass system Settings UI was saving to a Null save ref. The dynamic grass Loaded form a Null save Ref.

--- Build/Android/src/com/RTP/v7/OBBData.java
+++ Build/Android/src/com/RTP/v7/OBBData.java
@@ -19,7 +19,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "3", // the version of the APK that the file was uploaded against
-540357380L // the length of the file in bytes
+633762514L // the length of the file in bytes
 )
 };
 };
