8cbec2cb9400bcab7faf6d12f0aee2719b08d8ce
mag1182 <mag1182@rocketmail.com>
2022-06-19 06:08:03 -0500

    Updated Logic Core
    Added Not Or Nand Nor and counter

--- Source/ReturnToParadise/ReturnToParadise.Build.cs
+++ Source/ReturnToParadise/ReturnToParadise.Build.cs
@@ -10,7 +10,7 @@ public class ReturnToParadise : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "ChunkDownloader", "Http" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
